7d0a744cbc427d9e9a9a283c7709283a6036a9d9
Delano <delanoplacido@rogers.com>
2021-02-10 16:53:21 -0500

    Added Trip Title and Description

--- lib/tabs/trips/Trip.dart
+++ lib/tabs/trips/Trip.dart
@@ -10,6 +10,10 @@ class Trip extends StatefulWidget {
 }
 
 class _TripState extends State<Trip> {
+
+  final _formKey = GlobalKey<FormState>();
+  double _kilometreValue = 20;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,8 +60,68 @@ class _TripState extends State<Trip> {
         },
         body: Container(
           child: Column(
-            children: [
-              Text('Add form here'),
+            children: <Widget>[
+              // Trip Title TextField
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter a trip title',
+                ),
+                validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a trip title';
+                }
+                return null;
+              },
+            ), 
+            // Description TextField
+            TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter a trip description',
+                ),
+                validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter trip description';
+                }
+                return null;
+              },
+            ),
+            SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                activeTrackColor: Colors.green[700],
+                inactiveTrackColor: Colors.transparent,
+                trackShape: RectangularSliderTrackShape(),
+                trackHeight: 4.0,
+                thumbColor: Colors.lightGreenAccent,
+                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 12.0),
+                overlayColor: Colors.lightGreen.withAlpha(32),
+                overlayShape: RoundSliderOverlayShape(overlayRadius: 28.0),
+            ),
+              child: Slider(
+                min: 0,
+                max: 100,
+                divisions: 5,
+                label: '$_kilometreValue',
+                value: _kilometreValue,
+                onChanged: (value){
+                  setState(() {
+                    _kilometreValue = value;       
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Validate will return true if the form is valid, or false if
+                  // the form is invalid.
+                  if (_formKey.currentState.validate()) {
+                    // Process data.
+                  }
+                },
+                child: Text('Save Trip'),
+                ),
+              ),
             ],
           ),
         ),
