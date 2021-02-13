54c801074e2f6e0df0e46c16474de678f0a8de7f
TylerLindley <54914212+TylerLindley@users.noreply.github.com>
2021-02-12 21:52:27 -0500

    Adding the Add Trip form (#4)
    * Added Trip Title and Description
    
    * Creating an address textfieldform and a second duration textfield
    
    Inital CRUD Form (Not Finished)
    
    Co-authored-by: Delano <delanoplacido@rogers.com>

--- lib/tabs/trips/Trip.dart
+++ lib/tabs/trips/Trip.dart
@@ -10,6 +10,10 @@ class Trip extends StatefulWidget {
 }
 
 class _TripState extends State<Trip> {
+
+  final _formKey = GlobalKey<FormState>();
+  double _kilometreValue = 20;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,8 +60,98 @@ class _TripState extends State<Trip> {
         },
         body: Container(
           child: Column(
-            children: [
-              Text('Add form here'),
+            children: <Widget>[
+              // Trip Title TextField
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter a trip title',
+                ),
+                validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a trip title';
+                }
+                return null;
+              },
+            ), 
+            // Description TextField
+            TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter a trip description',
+                ),
+                validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter trip description';
+                }
+                return null;
+              },
+            ),
+            // Address TextField
+            // I'm not sure how else we are storing the location, geolocation would be cool
+            // but will we complete that near the end, and just store it as an address for now? -TL
+            TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Please enter an address',
+                ),
+                validator: (value) {
+                if (value.isEmpty) {
+                  return 'Valid address is needed';
+                }
+                return null;
+              },
+            ),
+            // Creating a TextFormField that uses the number keyboard for ease of access.
+            TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'How long is the trip? (in minutes)',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (String value) {
+
+                  int minutes = int.tryParse(value);
+
+                if (minutes == null || minutes <=0) {
+                  return 'Correct duration is needed';
+                }
+                return null;
+              },
+            ),
+            SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                activeTrackColor: Colors.green[700],
+                inactiveTrackColor: Colors.transparent,
+                trackShape: RectangularSliderTrackShape(),
+                trackHeight: 4.0,
+                thumbColor: Colors.lightGreenAccent,
+                thumbShape: RoundSliderThumbShape(enabledThumbRadius: 12.0),
+                overlayColor: Colors.lightGreen.withAlpha(32),
+                overlayShape: RoundSliderOverlayShape(overlayRadius: 28.0),
+            ),
+              child: Slider(
+                min: 0,
+                max: 100,
+                divisions: 5,
+                label: '$_kilometreValue',
+                value: _kilometreValue,
+                onChanged: (value){
+                  setState(() {
+                    _kilometreValue = value;       
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Validate will return true if the form is valid, or false if
+                  // the form is invalid.
+                  if (_formKey.currentState.validate()) {
+                    // Process data.
+                  }
+                },
+                child: Text('Save Trip'),
+                ),
+              ),
             ],
           ),
         ),
