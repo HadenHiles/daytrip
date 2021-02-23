6077b3c479d65a7350730233f33d16c3bf26b0c5
Tyler Lindley <TylerLindley13@gmail.com>
2021-02-23 11:05:18 -0500

    Resolve merge conflicts with tyler's+delano's latest changes

--- lib/tabs/Trips.dart
+++ lib/tabs/Trips.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Trips extends StatefulWidget {
@@ -14,6 +15,34 @@ class _TripsState extends State<Trips> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar( title: new Text('My Trips'),
+      ),
+      body: StreamBuilder(
+          // ignore: deprecated_member_use
+          stream:FirebaseFirestore.instance.collection('trips').doc(user.uid).collection('trips').snapshots(),
+          builder: (context, snapshot) {
+            if(!snapshot.hasData) return Text('Loading data... Please Wait...');
+            return ListView.builder( 
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index){
+                DocumentSnapshot trip = snapshot.data.documents[index];
+                  return ListTile(
+                    //Once we can store our pictures in Firebase, we can uncomment
+                    //and then our trips will start with the picture
+                    leading: Image.network(trip['img'], height: 50, width: 75,),
+                    title: Text(trip['title']),
+                    subtitle: Text(trip['description'].toString()),
+                    trailing: Text(trip['trip_distance'].toString()),
+                  );
+                });
+          }
+      )
+    );
+  }
+/*
+  @override
+  Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(left: 25, right: 25, bottom: 125),
       child: Column(
@@ -26,5 +55,5 @@ class _TripsState extends State<Trips> {
         ],
       ),
     );
-  }
+  }*/
 }
